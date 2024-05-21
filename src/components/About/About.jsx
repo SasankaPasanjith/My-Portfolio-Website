7fc52b70b9ffb77f328d283541cb3c888bbf393d
SasankaPasanjith <sasankapasanjith@gmail.com>
2024-05-21 20:00:07 +0530

    Added features to about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,17 +1,33 @@
 import React from 'react'
 import { getImageUrl } from '../../utils'
+import styles from "./About.module.css"
 
 export const About = () => {
   return (
-    <section>
-        <h2>About</h2>
-        <div><img src={getImageUrl("about/aboutImage.png")} alt="executive man"/>
-        <ul>
-            <li>
+    <section className={styles.container}>
+        <h2 className={styles.title}>About</h2>
+        <div className={styles.content}>
+          <img src={getImageUrl("about/aboutImage.png")} alt="executive man" className={styles.aboutImage}/>
+        <ul className={styles.aboutItems}>
+            <li className={styles.aboutItems}>
                 <img src={getImageUrl("about/cursorIcon.png")} alt ="cursor"/>
-                <div>
+                <div className={styles.aboutItemText}>
                     <h3>Frontend Development</h3>
-                    <p>I'm developing frontend using HTML, CSS, JavaScript & React</p>
+                    <p>I'm developing frontends using HTML, CSS, JavaScript & React</p>
+                </div>
+            </li>
+            <li className={styles.aboutItems}>
+                <img src={getImageUrl("about/serverIcon.png")} alt ="server"/>
+                <div className={styles.aboutItemText}>
+                    <h3>Backend Development</h3>
+                    <p>I'm developing backends using Java Springboot & python flask with APIs</p>
+                </div>
+            </li>
+            <li className={styles.aboutItems}>
+                <img src={getImageUrl("about/mlIcon.png")} alt ="ML Icon"/>
+                <div className={styles.aboutItemText}>
+                    <h3>Machine Learning Model Development</h3>
+                    <p>I'm developing ML models using libraries like Sklearn, Numpy, Pandas, keras, Tensorflow & etc. Also, I develop models for classification problems & NLP. </p>
                 </div>
             </li>
         </ul>
