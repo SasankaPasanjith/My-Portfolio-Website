f81eb79e96f4069556ceb32bcbc50b37acc1fcde
SasankaPasanjith <sasankapasanjith@gmail.com>
2024-05-22 19:48:10 +0530

    Added css styles for Technologies

--- src/components/Technologies/Technologies.jsx
+++ src/components/Technologies/Technologies.jsx
@@ -1,29 +1,31 @@
 import React from 'react'
+import styles from "./Technologies.module.css"
 import skills from "../../data/skills.json";
 import education from "../../data/education.json";
 import { getImageUrl } from '../../utils';
 
 export const Technologies = () => {
   return (
-    <section id="technologies">
-        <h2>Technologies & Education</h2>
-        <div>
-            <div>{
+    <section className={styles.container} id="Technologies">
+        <h2 className={styles.title}>Technologies & Education</h2>
+        <div className={styles.content}>
+            <div className={styles.skills}>{
                 skills.map((skill, id)=>{
-                    return<div key={id}>
-                        <div><img src={getImageUrl(skill.imageSrc)} alt ={skill.title}/>
+                    return(<div key={id} className={styles.skills}>
+                        <div className={styles.skillImageContainer}><img src={getImageUrl(skill.imageSrc)} alt ={skill.title}/>
                         </div>
                         <p>{skill.title}</p>
                     </div>
+                    )
                 })}
                 </div>
-            <ul>
+            <ul className={styles.history}>
                 {
                     education.map((educationItem, id) =>{
                         return(
-                        <li key={id}>
+                        <li key={id} className={styles.historyItem}>
                             <img src={getImageUrl(educationItem.imageSrc)} alt={`${educationItem.organisation} Logo`}/>
-                            <div>
+                            <div className={styles.historyItemDetails}>
                                 <h3>{`${educationItem.role}, ${educationItem.organisation}`}</h3>
                                 <p>{`${educationItem.startDate} - ${educationItem.endDate}`}</p>
                                 <ul>
