4244c4813a70c887719337b8fadf974b5f222d49
SasankaPasanjith <sasankapasanjith@gmail.com>
2024-05-22 15:31:27 +0530

    Create Technologies component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import styles from "./App.module.css";
 import { About } from "./components/About/About";
 import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
+import { Technologies } from "./components/Technologies/Technologies";
 
 function App() {
   return <div className={styles.App}>
   <Navbar/>
   <Hero/>
   <About/>
+  <Technologies/>
     </div>
 }
 
